bdb892d4e7649f432df02574c3eb203013fc791f
Joker2770 <1214220480@qq.com>
2021-02-14 20:12:40 +0800

    add site-packages. :arrow_up:

--- src/main.c
+++ src/main.c
@@ -495,7 +495,7 @@ bool init()
 					success = false;
 				}
 				//Initialize SDL_mixer
-				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 2048) < 0)
 				{
 					printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
 					success = false;
