04b658e9a9dfb3fe0df632402c25f8984d836a94
Joker2770 <1214220480@qq.com>
2021-02-26 21:34:29 +0800

    change title. :memo:

--- src/main.c
+++ src/main.c
@@ -448,7 +448,7 @@ bool init()
 		}
 
 		//Create window
-		gWindow = SDL_CreateWindow("Tetris-ever", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("Tetris-ever-mixer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
